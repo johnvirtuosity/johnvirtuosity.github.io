3a22008afa913d78e86f186884d0de89b1bdd797
johnvirtuosity <john.kan@virtuosity.co.nz>
2015-02-27 15:35:30 +1300

    Update to grocery list in JS

--- examples/groceryList/my_solution.js
+++ examples/groceryList/my_solution.js
@@ -30,11 +30,13 @@ var listItem = {
 */
 var listItem = {
   name: '',
-  qty: 0  
+  qty: 0,
+  cost: 0,
+  qty_purchased: 0  
 };
 
 //Set up array of hashs
-var listItems = [{name: 'bread', qty: 2},{name: 'eggs', qty: 2}];
+var listItems = [{name: 'bread', qty: 2, cost: 3.50, qty_purchased: 2},{name: 'eggs', qty: 2, cost: 2.15, qty_purchased: 1}];
 //var litItems = [];
 
 function displayList(mode){
@@ -48,10 +50,56 @@ function displayList(mode){
   };
 
   for (ptr ; ptr < listItems.length ; ptr++){
+    /*
+    //1. create the element you want to add
     var pElement = document.createElement('div');
+    //2. create a text element to append inside the pElement 
     var elementText = document.createTextNode(listItems[ptr]['qty'] + ' x ' + listItems[ptr]['name']);
+    //3. append the element text to the  element
     pElement.appendChild(elementText);
+    //4. Finally add the element to the actual document using appendChild
     document.getElementById('list').appendChild(pElement);
+    */
+    var divElement = document.createElement('div');
+    var elementText = document.createTextNode('');
+    divElement.appendChild(elementText);
+
+    
+    //  append text-boxes for the items name/qty/cost/qty_purchased
+    var nameElement = document.createElement('input');
+    var elementText = document.createTextNode('item name');
+    nameElement.value = listItems[ptr]['name'];
+    nameElement.appendChild(elementText);
+
+    //  append text-boxes for the items name/qty/cost/qty_purchased
+    var qtyElement = document.createElement('input');
+    var elementText = document.createTextNode('qty');
+    qtyElement.value = listItems[ptr]['qty'];
+    qtyElement.appendChild(elementText);
+
+    //  append text-boxes for the items name/qty/cost/qty_purchased
+    var costElement = document.createElement('input');
+    var elementText = document.createTextNode('dollars');
+    costElement.value = listItems[ptr]['cost'];
+    costElement.appendChild(elementText);
+
+    //  append text-boxes for the items name/qty/cost/qty_purchased
+    var qtypurchasedElement = document.createElement('input');
+    var elementText = document.createTextNode('bought');
+    qtypurchasedElement.value = listItems[ptr]['qty_purchased'];
+    qtypurchasedElement.appendChild(elementText);
+
+    
+    //Append the item details next
+    document.getElementById('list').appendChild(divElement);
+    document.getElementById('list').appendChild(nameElement);
+    document.getElementById('list').appendChild(qtyElement);
+    document.getElementById('list').appendChild(costElement);
+    document.getElementById('list').appendChild(qtypurchasedElement);
+
+
+
+
   };  
   //alert('My listItems : ' + listItems);
 };
@@ -61,7 +109,11 @@ function addItem(){
   var qtyToAdd = document.getElementById('qty').value;
 
   // Next add it to the array
-  listItems.push({name: itemToAdd, qty: qtyToAdd})
+  listItems.push({name: itemToAdd, qty: qtyToAdd, cost: 0, qty_purchased: 0})
+
+  //clear out the added item
+  document.getElementById('itemtoadd').value = '';
+  document.getElementById('qty').value = '';
 
   //refresh the list with just new item added
   displayList('add'); 
